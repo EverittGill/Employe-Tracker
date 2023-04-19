f8ba4ecbd29136109ea6a1162065ab7efa29aa49
EverittGill <eg.everittgill@gmail.com>
2023-04-19 10:33:23 -0400

    finalized the last functios to work properly

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 
 const inquirer = require('inquirer');
 const mysql = require('mysql2')
-// const fs = require('fs')
-// const query = require('./db/query');
-// const query = require('/db/query');
 
-// Function to prompt user for input and call the appropriate query
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -79,8 +58,8 @@ async function main() {
     return connection.promise().query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, department_id]);
   };
   
-  const updateEmployeeRole = (employee_id, role_id) => {
-    return connection.promise().query('UPDATE employee SET role_id = ? WHERE id = ?', [role_id, employee_id]);
+  const updateEmployeeRole = (employee_id, role_id, manager_id) => {
+    return connection.promise().query('UPDATE employee SET role_id = ?, manager_id = ? WHERE id = ?', [role_id, manager_id, employee_id]);
   };
   
 
@@ -114,29 +93,6 @@ async function main() {
       console.log('Department added successfully!');
       main()
       break;
-    // case 'Add a role':
-    //   const { title, salary, department_id } = await inquirer.prompt([
-    //     {
-    //       type: 'input',
-    //       name: 'title',
-    //       message: 'Enter the role title:',
-    //     },
-    //     {
-    //       type: 'number',
-    //       name: 'salary',
-    //       message: 'Enter the role salary:',
-    //     },
-    //     {
-    //       type: 'number',
-    //       name: 'department_id',
-    //       message: 'Enter the department ID for the role:',
-    //     },
-    //   ]);
-    //   await addRole(title, salary, department_id);
-    //   console.log('Role added successfully!');
-    //   main()
-    //   break;
-
     case 'Add a role':
       const departmentChoices = await viewAllDepartments();
       const departmentList = departmentChoices[0].map((department) => ({
@@ -214,23 +170,48 @@ async function main() {
   break;
 
     
+
     case 'Update an employee role':
-      const { employee_id, new_role_id } = await inquirer.prompt([
-        {
-          type: 'number',
-          name: 'employee_id',
-          message: 'Enter the employee ID to update:',
-        },
-        {
-          type: 'number',
-          name: 'new_role_id',
-          message: 'Enter the new role ID for the employee:',
-        },
-      ]);
-      await updateEmployeeRole(employee_id, new_role_id);
-      console.log('Employee role updated successfully!');
-      main()
-      break;
+  const updateRoleChoices = await viewAllRoles();
+  const updateRoleList = updateRoleChoices[0].map((role) => ({
+    name: `${role.title} (ID: ${role.id})`,
+    value: role.id,
+  }));
+
+  const updateEmployeeChoices = await viewAllEmployees();
+  const updateEmployeeList = updateEmployeeChoices[0].map((employee) => ({
+    name: `${employee.first_name} ${employee.last_name} (ID: ${employee.id})`,
+    value: employee.id,
+  }));
+
+  const updateInfo = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'employee_id',
+      message: 'Select the employee to update:',
+      choices: updateEmployeeList,
+    },
+    {
+      type: 'list',
+      name: 'new_role_id',
+      message: 'Select the new role for the employee:',
+      choices: updateRoleList,
+    },
+    {
+      type: 'list',
+      name: 'new_manager_id',
+      message: 'Select the new manager for the employee',
+      choices: updateEmployeeList,
+    }
+  ]);
+
+  await updateEmployeeRole(updateInfo.employee_id, updateInfo.new_role_id, updateInfo.new_manager_id);
+  console.log('Employee role and manager updated successfully!');
+  main();
+  break;
+
+
+
     case 'Exit':
       process.exit(0);
   }
